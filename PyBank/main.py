a241131c612173e8063ad1cba0e357f34410c0bd
rwoodill <woodillrachel@gmail.com>
2023-09-01 14:01:34 -0400

    Code Cleanup (PyBank)
    adding comments and ability to print out a txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,28 +9,23 @@ import sys
 # file extensions
 pybank_csv = "PyBank\\Resources\\budget_data.csv"
 analysis_txt = "PyBank\\Analysis\\FinancialAnalysis.txt"
-
+#variable to hold the total number of months analyzed
 total_months = 0
+#variable to hold the total profits 
 total_profits = 0
+#variable to hold the current value of the profits/losses
 value = 0
+#variable to hold the change in profits/losses
 change = 0
+#list to hold all the dates in the csv
 dates = []
+#list to hold all the profit/loss numbers in the csv
 profits = []
 
-# greatest_increase = 0
-# greatest_increase_index = 0
-# greatest_increase_date = "No Date Found"
-
-# greatest_decrease = 0
-# greatest_decrease_index = 0
-# greatest_decrease_date = "no Date Found"
-
-#avg_change = 0
-
-    # ----------------------------------------------------------
-    #  open the csv and read it, then store information about
-    #  the finances
-    # ----------------------------------------------------------
+# ----------------------------------------------------------
+#  open the csv and read it, then store information about
+#  the finances
+# ----------------------------------------------------------
 with open(pybank_csv, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     # skip header
@@ -42,30 +37,38 @@ with open(pybank_csv, "r") as csvfile:
     total_profits = total_profits + int(first_row[1])
     value = int(first_row[1])
 
-    # reader each row and create the lists
+    # read each row and create the lists
     for row in csvreader:
-        
         dates.append(row[0])
         # calculate change, then add to list of changes
         change = int(row[1]) - value
         profits.append(change)
+        #current value of profits/losses column
         value = int(row[1])
-
+        #increment total months
         total_months += 1
+        #add profit/loss to the total
         total_profits = total_profits + int(row[1])
 
+#calculate greatest increase from the list of profits (max)
 greatest_increase = max(profits)
+#find the index of greatest increase, then find date from index
 greatest_increase_index = profits.index(greatest_increase)
 greatest_increase_date = dates[greatest_increase_index]
 
+#calculate greatest decrease from list of profits (min)
 greatest_decrease = min(profits)
+#find the index of greatest decrease, then find date from index
 greatest_decrease_index = profits.index(greatest_decrease)
 greatest_decrease_date = dates[greatest_decrease_index]
 
+#calculate average change using the lists
 avg_change = sum(profits) / len(profits)
 
+# ----------------------------------------------------------
+#  Function to print the results to terminal
+# ----------------------------------------------------------
 def print_analysis():
-    #Displaying information
     print("Financial Analysis")
     print("----------------------------")
     print(f"Total Months: {str(total_months)}")
@@ -73,14 +76,14 @@ def print_analysis():
     print(f"Average Change: ${str(round(avg_change,2))}")
     print(f"Greatest Increase in Profits: {greatest_increase_date} (${str(greatest_increase)})")
     print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${str(greatest_decrease)})")
-    # ----------------------------------------------------------
-    #  Print the results to txt file  
-    #  simplified version ref:
-    #  https://stackoverflow.com/questions/23364096/how-to-write-output-of-terminal-to-file
-    # ----------------------------------------------------------
-    # with open(analysis_txt, "w") as text:
-    #     sys.stdout = text
-    #     print_results()
-    #print_results()
-
+# call print analysis
 print_analysis()
+
+# ----------------------------------------------------------
+#  Print the results to txt file  
+#  simplified version ref:
+#  https://stackoverflow.com/questions/23364096/how-to-write-output-of-terminal-to-file
+# ----------------------------------------------------------
+with open(analysis_txt, "w") as text:
+    sys.stdout = text
+    print_analysis()
